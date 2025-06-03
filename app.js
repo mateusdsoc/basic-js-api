3f7b864e99bf961d61ee4770744ce5a400a753b9
Mateus <mateuaraujo01@gmail.com>
2025-06-03 14:14:51 -0300

    connecting to env

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { json, urlencoded } from 'express';
 
 import {PORT} from './config/env.js';
 
@@ -6,12 +6,21 @@ import userRouter from './routes/users.route.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.router.js';
 import connectToDatabase from './database/mongoDB.js';
+import errorMiddleware from './middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();  
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser);
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+
+app.use(errorMiddleware);
 
 app.get('/', (req, res) => {
     res.send("Welcome to the subscriptiontrackerapi");
