5b1a1ea1bef4d25cbbe436aba5bc4b807d6916c7
Mateus <mateuaraujo01@gmail.com>
2025-06-09 15:11:39 -0300

    adding methods in controller

--- controller/subscription.controller.js
+++ controller/subscription.controller.js
@@ -1,3 +1,4 @@
+import { workflowClient } from "../config/upstash.js";
 import Subscription from "../models/subscription.model.js"
 
 export const createSubscription = async (req, res, next) => {
@@ -7,6 +8,17 @@ export const createSubscription = async (req, res, next) => {
             user: req.user._id,
             renewalDate: (req.body.starterDate + 30)
         });
+
+        const { workflowRunId } = await workflowClient.trigger({
+      url: `${SERVER_URL}/api/v1/workflows/subscription/reminder`,
+      body: {
+        subscriptionId: subscription.id,
+      },
+      headers: {
+        'content-type': 'application/json',
+      },
+      retries: 0,
+    })
 
         res.status(201).json({
             sucess:true,
