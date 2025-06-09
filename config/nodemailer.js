171b5ab785fc0d8ad56ba3aca34d6dbae7591cd8
Mateus <mateuaraujo01@gmail.com>
2025-06-09 15:44:30 -0300

    finishing all settings

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -2,10 +2,12 @@ import nodemailer from 'nodemailer';
 import {EMAIL_PASSWORD} from './env.js';
 
 export const accountEmail = "mateuaraujo01@gmail.com"
+
 const transported = nodemailer.createTransport({
     service: 'gmail', 
     auth:{
         user: accountEmail,
         pass: 'EMAIL_PASSWORD'
     }
-})
+});
+export default transported;
